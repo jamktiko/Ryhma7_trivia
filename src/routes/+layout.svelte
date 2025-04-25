0a84c9aae7871521a0a9bb3d997704cfa7d7d011
Jesse Kokki <ag2381@student.jamk.fi>
2025-04-25 11:45:56 +0300

    filujen lisäystä ja muokkausta

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import Header from '$lib/components/Header.svelte';
+	import Button from '$lib/components/Button.svelte';
+	interface Props {
+		children: Snippet;
+	}
+	let { children }: Props = $props();
+</script>
+
+<Header />
+<main>
+	{@render children()}
+</main>
---
+++ src/routes/Kategoria.svelte
@@ -0,0 +1,3 @@
+<script lang="ts">
+	import Button from '$lib/components/Button.svelte';
+</script>
