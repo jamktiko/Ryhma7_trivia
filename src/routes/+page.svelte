c158e1fa1f51e66882b91d86580e0882fa2b9db0
Jesse Kokki <ag2381@student.jamk.fi>
2025-04-28 14:02:01 +0300

    buttonin testausta, ei toimi vielä

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
 	import Button from '$lib/components/Button.svelte';
+
+	function moro() {
+		console.log('Moro!');
+	}
 </script>
 
 <!-- HTML tähän -->
@@ -12,7 +16,7 @@
 		<div>
 			<h1>Welcome to MindSpark!</h1>
 			<h2>Are you ready to test your knowledge?</h2>
-			<!-- <Button text="testi nappi" onclick={() => {}} /> -->
+			<!-- <Button text="testi nappi" /> -->
 		</div>
 	</body>
 </html>
