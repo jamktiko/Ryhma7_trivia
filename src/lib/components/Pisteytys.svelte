290a892486215d6545fb3a14b29c0630c3b89475
Jarius, Korpisaari <ag3328@student.jamk.fi>
2025-04-28 11:13:40 +0300

    lisätty pisteytys komponenttiin sisältö

--- src/lib/components/Pisteytys.svelte
+++ src/lib/components/Pisteytys.svelte
@@ -1,2 +1,33 @@
 <script lang="ts">
+let ajastin= 0;
+let tulos: number=0;
+
+function kellokay(){
+    ajastin= Date.now()
+}
+
+function laskepisteet( onkovastausoikein: boolean){
+    const aikaakului = (Date.now() - ajastin) / 1000; // Muunnetaan sekunneiksi
+    const maxpisteet= 150;
+    const maxaika=15; // vastausaika sekunteina
+    const aikasakko= maxpisteet/maxaika;  //pistevähennys joka sekunilta   
+    const vaaravastaus= 0;
+
+
+if (!onkovastausoikein){
+    tulos= vaaravastaus;} // ei pisteitä jos vastaus väärin
+else if 
+ (aikaakului > maxaika){
+    tulos= 0; // ei pisteitä, jos aika menee yli)
+} else{
+    tulos= Math.max(0, maxpisteet- aikaakului*aikasakko);
+}
+    
+}
+
 </script>
+
+
+
+
+
