607f693315b7c4421cb37cab2472899d9436d9c4
Essi Kaukometsa <ac7793@student.jamk.fi>
2025-04-29 10:51:02 +0300

    Lisätty fontit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,13 +16,13 @@
 
 <style>
 	:root {
-	--body-color: rgba(130, 79, 157, 30%);
-  --button1-color: rgba(253, 170, 83, 30%);
-  --buttontext-color: #411C5E;
-  --ansbutton1-color: rgba(160, 198, 207, 80%);
-  --ansbutton2-color: rgba(192, 155, 173, 90%);
-  --ansbutton3-color: rgba(65, 28, 94, 30%);
-  --ansbutton4-color: rgba(253, 170, 83, 50%);
+		--body-color: rgba(130, 79, 157, 30%);
+		--button1-color: rgba(253, 170, 83, 30%);
+		--buttontext-color: #411c5e;
+		--ansbutton1-color: rgba(160, 198, 207, 80%);
+		--ansbutton2-color: rgba(192, 155, 173, 90%);
+		--ansbutton3-color: rgba(65, 28, 94, 30%);
+		--ansbutton4-color: rgba(253, 170, 83, 50%);
 	}
 	:global body {
 		margin: 0;
@@ -31,12 +31,15 @@
 
 	h1 {
 		font-size: 55px;
+		font-family: 'Protest Strike', system-ui;
 	}
 	h2 {
 		font-size: 48px;
+		font-family: 'Protest Strike', system-ui;
 	}
 
 	h3 {
 		font-size: 40px;
+		font-family: 'KoHo', sans-serif;
 	}
 </style>
